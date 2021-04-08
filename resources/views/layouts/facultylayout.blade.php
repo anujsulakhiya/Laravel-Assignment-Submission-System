6da5ede88191d6b00811ee1de3fd4d1c2a926969
anujsulakhiya <anujsulakhiya@gmail.com>
2021-04-08 19:52:32 +0530

    side bar height fixed

--- resources/views/layouts/facultylayout.blade.php
+++ resources/views/layouts/facultylayout.blade.php
@@ -1,4 +1,5 @@
 <!doctype html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -24,6 +25,14 @@
     {{-- <script src="{{ asset('js/listbox.js') }}" defer></script> --}}
     <script type="text/javascript" src="{{ URL::asset('js/listbox.js') }}"></script>
 
+    <!-- Select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -42,31 +51,47 @@
 
 <body class="c-app">
 
-    <div id="wrapper">
+    <div id="wrapper" style="position: relative;">
         <!-- Sidebar -->
-        <div class="c-sidebar c-sidebar-dark c-sidebar-lg-show c-sidebar-show" id="sidebar-wrapper">
+        <div class="c-sidebar c-sidebar-dark c-sidebar-lg-show c-sidebar-show sidenav" id="sidebar-wrapper">
             <!-- <div class="sidebar-heading bg-danger text-light font-weight-bold">Assignment Submitter </div> -->
             <div class="c-sidebar-brand d-lg-down-none text-uppercase text-weight-bold">
                 Assignment Submitter
             </div>
             <div class="list-group-flush">
                 <ul class="c-sidebar-nav ps ps--active-y ">
-                    <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
-                            href="/home"><i class="fa fa-tachometer mr-2"></i>
+
+                    <li id="dashboard" class="c-sidebar-nav-item "><a
+                            class="c-nav-link list-group-item list-group-item-action" href="/home"><i
+                                class="fa fa-tachometer mr-2"></i>
                             Dashboard <span class="badge badge-info">NEW</span></a></li>
 
                     <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
                             href="/profile"><i class="fa fa-user mr-2"></i>
                             Profile</a></li>
+
+
+                    <li class="c-sidebar-nav-title">Enroll Students</li>
+                    <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
+                            href="/createbatch"><i class="fa fa-graduation-cap mr-2"></i>
+                            Create Class</a></li>
                     <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
                             href="/enrollstudent"><i class="fa fa-graduation-cap mr-2"></i>
                             Enroll Students</a></li>
+                    <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
+                            href="/enrollstudent"><i class="fa fa-graduation-cap mr-2"></i>
+                            Class Joining Requests </a></li>
+
+
+                    <li class="c-sidebar-nav-title">Assignment</li>
                     <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
                             href="/createassignment"><i class="fa fa-plus  mr-2"></i>
                             Create Assignment</a></li>
                     <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
                             href="/myassignment"><i class="fa fa-book mr-2"></i>
                             My Assignment</a></li>
+
+                    <li class="c-sidebar-nav-title">Other</li>
                     <li class="c-sidebar-nav-item "><a class="c-nav-link list-group-item list-group-item-action"
                             href="/"><i class="fa fa-recycle mr-2"></i>
                             Recycle Bin</a></li>
@@ -85,16 +110,15 @@
     </div>
 
     <div class="c-wrapper c-fixed-components ">
-        <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-            <span class="c-header-toggler-icon navbar-toggler-icon m-3" id="menu-toggle" width="97"
-                height="46"></span>
+        <header class="c-header c-header-light  c-header-with-subheader">
+            <span class="c-header-toggler-icon navbar-toggler-icon m-3" id="menu-toggle" width="97" height="46"></span>
             <div class="c-subheader px-3">
                 <!-- Breadcrumb-->
                 <x-breadcrumb breadcumb="" breadcumb1="" />
             </div>
         </header>
 
-        <div class="container-fluid" id="">
+        <div class="container-fluid" ied="mainpage">
 
             <div>
                 <main>
@@ -111,6 +135,15 @@
 
 </body>
 <script>
+    $(document).ready(function() {
+
+        $("#selUser").select2()
+
+        $("#dashboard").click(function() {
+            $("#mainpage").load("{{ URL::to('home') }}");
+        });
+    });
+
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
@@ -124,6 +157,13 @@
         document.execCommand("copy");
         alert("Copied the Link: " + copyText.value);
     }
+
+    $(document).ready(function() {
+
+        // Initialize select2
+        $("#sel1").select2();
+
+    });
 
 </script>
 
